b1200630d4eae4a9f8083894efae247297df646c
BenjaCimatti <benja.cimatti@gmail.com>
2021-07-07 19:57:10 -0300

    router agregado

--- comercio/urls.py
+++ comercio/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('producto/', ProductoViewSet.as_view({'get': 'list'})),
-    path('marca/', MarcaViewSet.as_view({'get': 'list'})),
-    path('categoria/', CategoriaViewSet.as_view({'get': 'list'})),
-    path('proveedor/', ProveedorViewSet.as_view({'get': 'list'})),
-    path('stock/', StockViewSet.as_view({'get': 'list'})),
-    path('carrito/', CarritoViewSet.as_view({'get': 'list'})),
-    path('productoagregado/', ProductoAgregadoViewSet.as_view({'get': 'list'}))
-]
+# urlpatterns = [
+#     path('producto/', ProductoViewSet.as_view({'get': 'list'})),
+#     path('marca/', MarcaViewSet.as_view({'get': 'list'})),
+#     path('categoria/', CategoriaViewSet.as_view({'get': 'list'})),
+#     path('proveedor/', ProveedorViewSet.as_view({'get': 'list'})),
+#     path('stock/', StockViewSet.as_view({'get': 'list'})),
+#     path('carrito/', CarritoViewSet.as_view({'get': 'list'})),
+#     path('producto-agregado/', ProductoAgregadoViewSet.as_view({'get': 'list'}))
+# ]
+
+router = DefaultRouter()
+router.register("producto", ProductoViewSet, basename="producto")
+router.register("marca", MarcaViewSet, basename="marca")
+router.register("categoria", CategoriaViewSet, basename="categoria")
+router.register("proveedor", ProveedorViewSet, basename="proveedor")
+router.register("stock", StockViewSet, basename="stock")
+router.register("carrito", CarritoViewSet, basename="carrito")
+router.register("producto-agregado", ProductoAgregadoViewSet, basename="producto-agregado")
+
+urlpatterns = router.urls
